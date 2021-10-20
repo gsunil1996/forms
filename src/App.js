101260397dbe5e7997cd1c9d6f004294d25b6d93
gsunil1996 <sunil_nj2_085@masai.school>
2021-10-20 23:44:43 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import EmployeeDetails from './FormData/EmployeeDetails';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Employee Data</h1>
+      <EmployeeDetails />
+    </div>
+  );
+}
+
+export default App;
